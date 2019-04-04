92d4b6f6be2938661d6d329757c1db7848b79882
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2019-04-04 11:28:03 -0700

    Rename .alias.sh -> alias.sh

--- alias.sh
+++ alias.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 # alias for starting an emacs client for a particular dir/file
 alias ec='function emacs_client() { emacsclient --no-wait -c -a emacs "$@" > /dev/null 2>&1 &; }; emacs_client'
