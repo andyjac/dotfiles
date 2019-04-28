ca2ce7ef50d4e09916850e96936d1f1c3ab6a475
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2019-04-28 09:20:04 -0400

    Make variables local, modify log output

--- installers/shared/git.sh
+++ installers/shared/git.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 git_clone() {
-  repo=$1
-  dest=$2
+  local repo=$1
+  local dest=$2
 
   if [ $(file_exists $dest) == "t" ]; then
     log_action_message "found repo" "$repo ($dest)"
   else
-    log_action_message "clone" $repo
+    log_action_message "clone" "$repo -> $dest"
     git clone $repo $dest > /dev/null 2>&1
   fi
 }
