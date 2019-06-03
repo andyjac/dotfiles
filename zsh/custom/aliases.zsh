2cd66140923c7803ca94de5d545e265c1b9e628d
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2019-06-03 10:23:18 -0700

    Update aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -12,10 +12,18 @@ alias hegrabuild='clear && cd ~/code/igneous/mesa/ui && export DEV_STATIC=true &
 alias hegraserve='cd $HOME/code/igneous/mesa/static/mantle/tools/searchindexer && http-server -p 8001 --cors -c-1 -a localhost'
 alias hegrastart='hegrabuild && hegraserve'
 
+# shorter hegra aliases
+alias hb='hegrabuild'
+alias hh='hegraserve'
+alias ha='hegrastart'
+
 # setup tunnel for gamma
 alias gammatunnel='ssh -f -N -g -D 6666 aws-dev'
 
-# build mesa from scratch
+# igneous build commands
 alias mesabuild='make clean_images && export DEV_STATIC=true && time make wipe clean all topo12 LITHOS_DISK_SIZE=9000 start-container-services'
+alias ctopobuild='make wipe all ctopo bosra-slab deploy-bosra-container-services cloud-bosra TOPO_NAME=btopo1'
 
-alias datasetdetails-local='echo "http://localhost:3000/ux/dataset-details?exportId=2&systemName=35.163.132.140&exportName=/home/nfs/oldmiss2"'
+local dataset_details_local_url="http://localhost:3000/ux/dataset-details?exportId=2&systemName=35.163.132.140&exportName=/home/nfs/oldmiss2"
+
+alias dd-url='echo $dataset_details_local_url | pbcopy && echo "copied!"'
